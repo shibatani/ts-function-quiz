4b8a44627bfbef25a7af1dcca79fd5ae7838eaa0
shibatani <64350081+shibatani@users.noreply.github.com>
2022-05-12 18:43:50 +0900

    FizzBuzzを関数化させる

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import "./styles.css";
+// FizzBuzzの処理を関数化させる
+function getFizzBuzzString(i: number): string {
+  if (i % 3 === 0 && i % 5 === 0) {
+    return "FizzBuzz";
+  } else if (i % 3 === 0) {
+    return "Fizz";
+  } else if (i % 5 === 0) {
+    return "Buzz";
+  } else {
+    return String(i);
+  }
+}
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+// 1〜100が入った配列を作る
+function sequence(max: number): number[] {
+  return [...Array(max).keys()].map((i) => ++i);
+}
+
+for (const i of sequence(100)) {
+  const message = getFizzBuzzString(i);
+  console.log(message);
+}
